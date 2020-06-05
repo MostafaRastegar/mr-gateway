8b9f76aa2de033c64dc32756c210091dd1f9b0a0
mostafa rastegar <m.rastegar991@gmail.com>
2020-06-06 00:06:11 +0430

    WIP: add uuid to user verify

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,26 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+const { v4: uuidv4 } = require("uuid");
+dotenv.config();
+const { ACCESS_TOKEN } = process.env;
 
+const users = [
+  {
+    username: "john",
+    password: "password123admin",
+    role: "admin",
+    token: "youraccesstokensecret",
+  },
+  {
+    username: "anna",
+    password: "password123member",
+    role: "member",
+    token: "youraccesstokensecret",
+  },
+];
+
+var router = express.Router();
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
@@ -8,27 +28,68 @@ router.get("/", function (req, res, next) {
 
 /* GET users listing. */
 router.post("/bank", function (req, res, next) {
-  const { orderId, token, backLink, price } = req.body;
+  const { orderId, token, backLink, price, username, password } = req.body;
   console.log({
     orderId,
     token,
     backLink,
     price,
+    username,
+    password,
   });
-  // res.redirect(backLink);
-  res.render("bank", { title: "Bank gateway", orderId, token, price });
-});
-// router.get('/bank/:id/:token', function(req, res, next) {
-//   const { id,token, backLink } = req.params;
-//   // res.redirect(backLink);
-//   res.render('bank', { title: 'Users', id,token });
-//
-// });
+  // Filter user from the users array by username and password
+  const user = users.find((u) => {
+    return u.username === username && u.password === password;
+  });
 
-// router.post('/user/submit', function(req, res, next) {
-//   const { id } = req.body;
-//   console.log(id);
-//   res.redirect(`/users/${id}`);
+  if (user && user.token === ACCESS_TOKEN) {
+    const refId = uuidv4();
+    const result = {
+      refId,
+      orderId,
+      backLink,
+      saleOrderId: orderId,
+      SaleReferenceId: Math.floor(Math.random() * 10000000)
+    }
+    res.json(result);
+  } else {
+    res.send("Username or password incorrect");
+  }
+  // res.redirect(backLink);
+  res.render("bank", { title: "Bank gateway", ...result });
+});
+
+// router.post("/login", (req, res) => {
+//   // Read username and password from request body
+//   const { username, password, token } = req.body;
+//
+//   // Filter user from the users array by username and password
+//   const user = users.find((u) => {
+//     return u.username === username && u.password === password;
+//   });
+//
+//   console.log({
+//     ACCESS_TOKEN,
+//     user
+//   })
+//
+//   if (user && user.token === ACCESS_TOKEN) {
+//     // Generate an access token
+//     const accessToken = jwt.sign(
+//       { username: user.username, role: user.role },
+//       ACCESS_TOKEN
+//     );
+//     const refId = uuidv4(null,null,5);
+//
+//     // res.json({
+//     //   accessToken,
+//     // });
+//     res.json({
+//       refId
+//     });
+//   } else {
+//     res.send("Username or password incorrect");
+//   }
 // });
 
 module.exports = router;
